eebab6a5e963e575c3392eb676d558209131964c
Abhigyan Chattopadhyay <51954245+abhigyan2001@users.noreply.github.com>
2022-02-27 20:14:09 +0530

    changed tokenization file

--- Assignment1/tokenization.py
+++ Assignment1/tokenization.py
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+from util import *
+
+# Add your import statements here
+import re
+
+
+
+class Tokenization():
+
+	def naive(self, text):
+		"""
+		Tokenization using a Naive Approach
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = [[] for i in range(len(text))]
+
+		#Fill in code here
+		for i,sentence in enumerate(text):
+			sentence = re.sub('\'|\"|\(|\)|,|;|\[|\]|\{|\}|\.|\?|!', ' ', sentence).strip()
+			tokenizedText[i] = sentence.split()
+		return tokenizedText
+
+	def pennTreeBank(self, text):
+		"""
+		Tokenization using the Penn Tree Bank Tokenizer
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		return tokenizedText
+
+# txt = ["nlp,(txt) 'lol' \"lmao\" [asdasd]{asda}]} "]
+# print(Tokenization().naive(txt))
+=======
 from util import *
 
 # Add your import statements here
@@ -73,3 +128,4 @@ class Tokenization():
 			tokenizedText.append(tokens)
 
 		    return tokenizedText
+>>>>>>> 217a109d6f1bdfc52ed38af5431895fc38c0fca0
